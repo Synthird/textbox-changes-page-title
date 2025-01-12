fab604e2c4b44f936c3554a3996cc65081720ae0
Synthird <105394020+Synthird@users.noreply.github.com>
2025-01-12 14:50:14 -0800

    Format code with a formatter

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const title = document.getElementById("title");
 const changeTitle = document.getElementById("changeTitle");
 
-changeTitle.onkeyup = function() {
+changeTitle.onkeyup = function () {
 	title.textContent = changeTitle.value;
 }
