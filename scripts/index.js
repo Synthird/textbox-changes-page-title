511b34fcf430919e76c6fc9a537d97f480fc9bf9
Synthird <105394020+Synthird@users.noreply.github.com>
2025-01-12 15:22:08 -0800

    Removed console.log lines

--- scripts/index.js
+++ scripts/index.js
@@ -5,10 +5,8 @@ const defaultTitle = title.textContent;
 
 changeTitle.onkeyup = function () {
 	if (!changeTitle.value.replaceAll(" ", "") == "") {
-		console.log("Not empty")
 		title.textContent = changeTitle.value;
 	} else {
-		console.log("Empty")
 		title.textContent = defaultTitle;
 	}
 }
