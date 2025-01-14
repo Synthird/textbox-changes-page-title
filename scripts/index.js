8ba7715e74d5762e1f44b370e6f569fa313a5d60
Synthird <105394020+Synthird@users.noreply.github.com>
2025-01-13 17:01:57 -0800

    Added a copy code feature

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,33 @@
 const title = document.getElementById("title");
 const changeTitle = document.getElementById("changeTitle");
+const titleCode = document.getElementById("htmlTitleCode");
+const copyCode = document.getElementById("copyCode");
+const copyNotify = document.getElementById("copyNotify");
 
 const defaultTitle = title.textContent;
+
+function setCodeToDefault() {
+	titleCode.textContent = `<title>${defaultTitle}</title>`;
+}
+
+function clearCopyNotify() {
+	copyNotify.textContent = "";
+}
+
+setCodeToDefault();
 
 changeTitle.onkeyup = function () {
 	if (!changeTitle.value.replaceAll(" ", "") == "") {
 		title.textContent = changeTitle.value;
+		titleCode.textContent = `<title>${changeTitle.value}</title>`
 	} else {
 		title.textContent = defaultTitle;
+		setCodeToDefault();
 	}
+}
+
+copyCode.onclick = function () {
+	navigator.clipboard.writeText(titleCode.textContent);
+	copyNotify.textContent = "HTML code has been copied to clipboard!";
+	setTimeout(clearCopyNotify, 1300);
 }
