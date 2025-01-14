14a3d9219e25b5273148bbaf87a5c73b5c9cc61d
Synthird <105394020+Synthird@users.noreply.github.com>
2025-01-13 18:20:58 -0800

    A variable has been renamed to match an element id value

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 const title = document.getElementById("title");
 const changeTitle = document.getElementById("changeTitle");
-const titleCode = document.getElementById("htmlCode");
+const htmlCode = document.getElementById("htmlCode");
 const copyCode = document.getElementById("copyCode");
 const copyNotify = document.getElementById("copyNotify");
 
 const defaultTitle = title.textContent;
 
 function setCodeToDefault() {
-	titleCode.textContent = `<title>${defaultTitle}</title>`;
+	htmlCode.textContent = `<title>${defaultTitle}</title>`;
 }
 
 function clearCopyNotify() {
@@ -19,7 +19,7 @@ setCodeToDefault();
 changeTitle.onkeyup = function () {
 	if (!changeTitle.value.replaceAll(" ", "") == "") {
 		title.textContent = changeTitle.value;
-		titleCode.textContent = `<title>${changeTitle.value}</title>`
+		htmlCode.textContent = `<title>${changeTitle.value}</title>`
 	} else {
 		title.textContent = defaultTitle;
 		setCodeToDefault();
@@ -27,7 +27,7 @@ changeTitle.onkeyup = function () {
 }
 
 copyCode.onclick = function () {
-	navigator.clipboard.writeText(titleCode.textContent);
+	navigator.clipboard.writeText(htmlCode.textContent);
 	copyNotify.textContent = "HTML code has been copied to clipboard!";
 	setTimeout(clearCopyNotify, 1300);
 }
